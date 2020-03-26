e2bf1b9c4f7cd333e1d743aafb42e43cbc43e993
MonyaMya <61384814+MonyaMya@users.noreply.github.com>
2020-03-26 07:32:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Curriculum Vitae</title>
+</head>
+
+<body>
+    <header>
+          
+        
+        <!--Navbar-->
+        
+        <nav class="navbar">
+            <div class="nav-item-burger">
+                <a class="nav-link" href="#burger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </a>
+            </div>
+            <div class="menu_mobile" id="burger">
+                <div class="nav-item">
+                    <a class="nav-link" href="#presentation">Who am I?</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#journey">Relevant Experience</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#formulary">Education</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#skills">Skills</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#hobbies">Hobbies</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#formulary">Contact</a>
+                </div>
+            </div>
+            <div class="nav-item-lang">
+                <a class="nav-link" href="#menu_lang"><i class="em em-gb"></i></a>
+            </div>
+            <div class="nav-menu_lang" id="menu_lang">
+                <a href="#"><i class="em em-gb"></i></a>
+                <a href="#"><i class="em em-fr"></i></a>
+                <a href="#"><i class="em em-it"></i></a>
+                <a href="#"><i class="em em-de"></i></a>
+                <a href="#"><i class="em em-es"></i></a>
+                <a href="#"><i class="em em-jp"></i></a>
+            </div>
+        </nav>
+    </header>
+    
+    
+    <main>
+
+        <section id="presentation">
+            <!--Presentation-->
+            
+        </section>
+        
+        
+        
+        
+        <section id="journey">
+            <!--Journey-->
+
+        </section>
+  
+  
+  
+  
+        <section id="skills">
+            <!--Skills-->
+
+        </section>
+        
+        
+        
+        
+        <section id="hobbies">
+            <!--Hobbies-->
+         
+        </section>
+        
+        
+        
+        
+        <section id="formulary">
+            <!--Contact-->
+            
+        </section>
+    </main>
+    
+    
+    <footer>
+        <!--Footer-->
+
+    </footer>
+    
+    
+</body>
+
+</html>
